c75e6ac230143206d14c363d32be479ee0a158ac
Adriano-usina <adriano.oliveira@colorado.com.br>
2025-08-08 16:10:26 -0300

    Add files via upload

---
+++ fornecedores.js
@@ -0,0 +1,20 @@
+export default function Fornecedores() {
+  return (
+    <div>
+      
+<nav style={{ padding: '10px', background: '#eee' }}>
+  <a href="/painel">Painel</a> | 
+  <a href="/fornecedores">Fornecedores</a> | 
+  <a href="/veiculos">Veículos</a> | 
+  <a href="/fretes">Fretes</a> | 
+  <a href="/coletas">Coletas</a> | 
+  <a href="/entregas">Entregas</a> | 
+  <a href="/relatorios">Relatórios</a> | 
+  <a href="/login">Login</a>
+</nav>
+
+      <h1>Fornecedores</h1>
+      <p>Conteúdo da página de fornecedores.</p>
+    </div>
+  );
+}
