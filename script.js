454a274a12d80929e977afbb4152352f1cbc4064
Adriano-usina <adriano.oliveira@colorado.com.br>
2025-08-08 14:32:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function mostrarCotacao() {
-  document.getElementById('resultado').innerText = 'R$ 154,90 - Cotação Simulada';
-}
+
+document.getElementById('freteForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const origem = document.getElementById('origem').value;
+  const destino = document.getElementById('destino').value;
+  const peso = parseFloat(document.getElementById('peso').value);
+
+  const precoPorKg = 2.50;
+  const frete = peso * precoPorKg;
+
+  const resultado = `De ${origem} para ${destino}, o valor do frete é R$ ${frete.toFixed(2)}.`;
+  document.getElementById('resultado').textContent = resultado;
+});
